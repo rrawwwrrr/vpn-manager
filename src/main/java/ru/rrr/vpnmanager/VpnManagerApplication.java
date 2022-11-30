9156e0c0808d9e0f3d6ec2d9115ac3a45c4e8bdc
rrawwwrrr <39938067+rrawwwrrr@users.noreply.github.com>
2022-11-30 20:47:08 +0400

    spring init

---
+++ src/main/java/ru/rrr/vpnmanager/VpnManagerApplication.java
@@ -0,0 +1,13 @@
+package ru.rrr.vpnmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VpnManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VpnManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/rrr/vpnmanager/VpnManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.rrr.vpnmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VpnManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
